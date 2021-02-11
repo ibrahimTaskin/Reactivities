f35b854520d3878b76491a4fc83fa0e177c2dd61
ibrahimtaskin <taskin.ibrahim1@gmail.com>
2021-02-11 13:16:54 +0300

    Seed database

--- API/Program.cs
+++ API/Program.cs
@@ -14,29 +14,30 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var host=CreateHostBuilder(args).Build();
-            
-            using (var scope=host.Services.CreateScope())
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
             {
-                var services=scope.ServiceProvider;
+                var services = scope.ServiceProvider;
                 try
                 {
                     // Program başlamadan Migration var mı kontrol ediyoruz.
                     // Varsa Migrate ediyoruz
-                    var context=services.GetRequiredService<DataContext>();
-                    context.Database.Migrate();
+                    var context = services.GetRequiredService<DataContext>();
+                    await context.Database.MigrateAsync();
+                    await Seed.SeedData(context);
                 }
                 catch (Exception ex)
                 {
                     // Log servisine Migration error ları yazıyoruz.
-                    var logger=services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex,"An error occured during migration");
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured during migration");
                 }
             }
 
-            host.Run();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

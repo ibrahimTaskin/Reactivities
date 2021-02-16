45de46249341eef0d562508ba86997dd2dae3805
ibrahimtaskin <taskin.ibrahim1@gmail.com>
2021-02-16 11:44:33 +0300

    Handle Axios agent , added loading component

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosResponse } from "axios";
+import { Activity } from "../models/activity";
+
+const sleep = (delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+};
+
+axios.defaults.baseURL = "http://localhost:5000/api";
+
+axios.interceptors.response.use(async (response) => {
+  try {
+        await sleep(1000);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return await Promise.reject(error);
+    }
+});
+// dönen datayı alıyoruz.
+// Type safety için generic Type
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+const requests = {
+  // url ile get,post,put,delete
+  get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+  post: <T>(url: string, body: {}) =>
+    axios.post<T>(url, body).then(responseBody),
+  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+  del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+};
+
+const Activities = {
+  list: () => requests.get<Activity[]>("/activities"),
+};
+
+const agent = {
+  Activities,
+};
+
+export default agent;
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,27 +1,41 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Container} from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import Navbar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from "uuid";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
   // useState<Activity[]> Activity array i şeklinde alıyoruz.
   const [activities, setActivities] = useState<Activity[]>([]); // array verdiğimiz için array almalı
-  const [selectedActivity, setSelectedActivity] = useState<
-    Activity | undefined
-  >(undefined); //Activity veya undenifed gelebilir.
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined); //Activity veya undenifed gelebilir.
   const [editMode, setEditMode] = useState(false);
+  const[loading,setLoading]=useState(true);
 
   useEffect(() => {
     //axios.get<Activity[]> ,Herhangi bir type vermeyebiliriz. Fakat Activity istiyoruz
     //Amaç Type Safety
-    axios
-      .get<Activity[]>("http://localhost:5000/api/activities")
-      .then((response) => {
-        setActivities(response.data);
+    //   axios
+    //     .get<Activity[]>("http://localhost:5000/api/activities")
+    //     .then((response) => {
+    //       setActivities(response.data);
+    //     });
+
+    // Yazdığımız agent ile kullanım
+    agent.Activities.list().then((response) => {
+      // listeyi boş bir array a aldık.Foreach ile döndük ve tarih'ten saati attık.
+      // activities.push ile tekrar aldık. ve setActivities ile geri döndük.
+      let activities: Activity[] = [];
+      response.forEach((activity) => {
+        activity.date = activity.date.split("T")[0];
+        activities.push(activity);
       });
+      setActivities(activities);
+      // Activities ler geldikten sonra false
+      setLoading(false);
+    });
   }, []);
 
   function handleSelectActivity(id: string) {
@@ -40,18 +54,23 @@ function App() {
     setEditMode(false);
   }
 
-  function handleCreateOrEditActivity(activity:Activity) {
-    activity.id 
-    ? setActivities([...activities.filter(x=>x.id !== activity.id),activity]) //Eğer id'ye sahip entity varsa getir. Düzenle
-    : setActivities([...activities,{...activity,id:uuid()}]);  // Eğer id yoksa yeni bir Guid ile entity oluştur.
+  function handleCreateOrEditActivity(activity: Activity) {
+    activity.id
+      ? setActivities([
+          ...activities.filter((x) => x.id !== activity.id),
+          activity,
+        ]) //Eğer id'ye sahip entity varsa getir. Düzenle
+      : setActivities([...activities, { ...activity, id: uuid() }]); // Eğer id yoksa yeni bir Guid ile entity oluştur.
 
     setEditMode(false);
     setSelectedActivity(activity);
   }
 
-  function handleDeleteActivity(id:string) {
-    setActivities([...activities.filter(x=>x.id!==id)]);
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter((x) => x.id !== id)]);
   }
+
+if (loading) return <LoadingComponent  content='Loading App...'/>
 
   return (
     <>

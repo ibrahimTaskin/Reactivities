2bfa349c0956bfbca2b587c91f6c6724915c1ebd
ibrahimtaskin <taskin.ibrahim1@gmail.com>
2021-02-19 15:53:10 +0300

    Styling App and HomePage

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -16,9 +16,23 @@ export default class ActivityStore {
     makeAutoObservable(this);
   }
 
+  // Computed Function
   get activitiesByDate() {
     return Array.from(this.activityRegistry.values()).sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+  }
+
+  // Computed Function
+  get groupedActivities(){
+    return Object.entries(
+      this.activitiesByDate.reduce((activities,activity)=>{
+        const date=activity.date; // tarihlere göre sıralamak için bizim key'imiz olacak.
+        activities[date]=
+        activities[date] ? [...activities[date],activity]
+                         : [activity];
+          return activities;
+      },{} as {[key:string]:Activity[]}) // string Key'li Activity dönüyor.
     );
   }
 
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Container } from 'semantic-ui-react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
 
 function HomePage() {
-    return (
-        <Container style={{marginTop:'7em'}}>
-            <h1>Home Page</h1>
-            <h3> <Link to='/activities'>Activities</Link></h3>
-        </Container>
-    )
+  return (
+    <Segment inverted textAlign="center" vertical className="masthead">
+      <Container text>
+        <Header as="h1" inverted>
+          <Image size="massive" src="/assets/logo.png" alt="logo" style={{marginBottom:12}}/>Reactivities
+        </Header>
+        <Header as='h2' inverted content='Hoşgeldiniz'/>
+        <Button as={Link} to='/activities' size='huge' inverted>Beni Aktivitelere Götür</Button>
+      </Container>
+    </Segment>
+  );
 }
 
-export default HomePage
+export default HomePage;
